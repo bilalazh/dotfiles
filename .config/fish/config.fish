482723ec883479294bd1b0fadca5db9e2d251786
bilalazh <heybilalazhar@gmail.com>
2026-03-16 01:08:40 +0500

    FISH: Fix wall alias path to use hypr images directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ alias vivaldi 'vivaldi --ozone-platform=x11'
 alias vivaldi-browser 'vivaldi --ozone-platform=x11'
 alias zen '/home/bi/.local/bin/zen'
 alias obsidian 'obsidian &'
-alias wall 'pkill swaybg; swaybg -i (find ~/Pictures/Wallpapers/ -type f | shuf -n 1) -m fill &'
+alias wall 'pkill swaybg; swaybg -i (find /home/bi/dotfiles/.config/hypr/images/ -type f | shuf -n 1) -m fill &'
 
 
 
