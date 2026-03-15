09b1864db24b29b09d6eb7672fe422a76837ebb7
bilalazh <heybilalazhar@gmail.com>
2026-03-16 00:37:25 +0500

    FISH: Add cll function for recent cliphist entries

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 
 alias gemini 'env NODE_OPTIONS="--dns-result-order=ipv4first" command gemini'
+alias vivaldi 'vivaldi --ozone-platform=x11'
 alias vivaldi-browser 'vivaldi --ozone-platform=x11'
 alias zen '/home/bi/.local/bin/zen'
 alias obsidian 'obsidian &'
@@ -18,12 +19,16 @@ end
  set -g fish_greeting
 
 
- function cw
-     cliphist wipe
-     # Optional:
-      notify-send "Clipboard Cleared"
-      # uncomment if you want a desktop notification
+function cw
+    cliphist wipe
+    # Optional:
+     notify-send "Clipboard Cleared"
+     # uncomment if you want a desktop notification
  end
+
+function cll
+    cliphist list | cut -f2- | head -n 50
+end
 
 
 	# Commands to run in interactive sessions can go here
