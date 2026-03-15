3fb0c5fdf48cd6a71f8015cd469692b0bf8de00d
bilalazh <heybilalazhar@gmail.com>
2026-03-16 01:08:05 +0500

    FISH: Add wall alias to set random wallpaper

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ alias vivaldi 'vivaldi --ozone-platform=x11'
 alias vivaldi-browser 'vivaldi --ozone-platform=x11'
 alias zen '/home/bi/.local/bin/zen'
 alias obsidian 'obsidian &'
+alias wall 'pkill swaybg; swaybg -i (find ~/Pictures/Wallpapers/ -type f | shuf -n 1) -m fill &'
 
 
 
